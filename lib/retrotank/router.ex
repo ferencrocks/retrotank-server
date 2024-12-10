6aa65e789dd38bd4bdc8d5582711ec20175e0fc6
Faluvegi Ferenc <frantzdae@live.com>
2024-12-10 22:30:35 +0200

    Plug router JSON parser

--- lib/retrotank/router.ex
+++ lib/retrotank/router.ex
@@ -2,9 +2,19 @@ defmodule Retrotank.Router do
   use Plug.Router
 
   plug :match
+  plug Plug.Parsers,
+     parsers: [:urlencoded, :json],
+     json_decoder: Jason
   plug :dispatch
 
   get "/" do
+    send_resp(conn, 200, "Hello Maxik")
+  end
+
+  post "/player/join" do
+    %{:body_params => body_params} = conn
+
+    IO.inspect body_params
     send_resp(conn, 200, "Hello Maxik")
   end
 
